f3716a586529068f99ef64e817a6661078d5e133
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-08-11 18:57:01 -0400

    updated map on detail page.

--- src/resources/detail.js
+++ src/resources/detail.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import L from 'leaflet';
-import { GeoJSON, Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import {isEmpty} from 'lodash';
 import {ResourcePublishDate} from './../utils/publish_date';
 import md from 'marked';
+import GeofocusMap from './../geofocus_map';
 import './detail.css';
-
-const map_bounds = [-73.50821,42.886778,  -69.858861, 41.187053];
-
-const GeofocusesMap = (props) => 
-  <Map bounds={props.bounds}>
-    {props.geofocuses.map( (json, indx) => {
-      return <GeoJSON key={indx} data={json} />
-    })}
-  </Map>;
 
 const ContentTypes = (props) => {
   return <div className='content-types'>
@@ -25,12 +15,12 @@ const ContentTypes = (props) => {
         })}
         </div>;
     })}
-  </div> 
+  </div>
 
 };
 
 const Subsection = (props) => {
-  return <div key={`subsection-${props.name}`} className='subsection'>
+  return props.items.length > 0 ? <div key={`subsection-${props.name}`} className='subsection'>
     <label>{props.name}</label>
     <div className='items'>
       {props.items.map( (item, indx) => {
@@ -39,21 +29,21 @@ const Subsection = (props) => {
             </div>;
         })}
     </div>
-  </div>
+  </div> : null;
 };
- 
+
 class ResourcesDetailPage extends Component {
   componentDidMount() {
     this.props.load_resource(this.props.match.params.docid);
   }
 
-  componentWillReceiveProps(nextProps) { 
+  componentWillReceiveProps(nextProps) {
     this.props.load_resource(this.props.match.params.docid);
   }
 
   render() {
     let resource = this.props.resources_result || {};
-    let geofocus_json = this.props.geofocus_json;
+
     if (isEmpty(resource)) {
       return null;
     } else {
@@ -63,7 +53,7 @@ class ResourcesDetailPage extends Component {
           <ContentTypes content_types={resource.content_types} />
           <h1>{resource.title}</h1>
           <h5>{resource.subtitle}</h5>
-          <hr/> 
+          <hr/>
 
           {resource.authors.length === 0 ? null :
             <div className='authors'>
@@ -81,13 +71,17 @@ class ResourcesDetailPage extends Component {
                 })}
             </div>}
 
-          {resource.image ? 
+          {resource.image ?
             <img alt={resource.title + ' example'} src={resource.image} className='col-12 col-md-5 float-right'/>
             : null}
 
-            {geofocus_json.length > 0 ? <GeofocusesMap geofocuses={geofocus_json} /> : null}
 
-            <div className='content' dangerouslySetInnerHTML={{__html: md(resource.content || "")}}></div>
+          <div className='content' dangerouslySetInnerHTML={{__html: md(resource.content || "")}}></div>
+          <hr/>
+
+          {resource.geofocuses.length > 0 ? <div className='resource-map'>
+                <GeofocusMap geofocuses={resource.geofocuses || []} />
+              </div> : null }
 
             <Subsection name='Actions' facet="actions" items={resource.actions} />
             <Subsection name='Climate Changes' facet="climate_changes" items={resource.climate_changes} />
