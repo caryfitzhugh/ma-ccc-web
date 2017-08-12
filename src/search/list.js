160a90e6356f9f113b82bb27b1baa6c8787028d1
jmargolisvt <jeff@jmargolisvt.com>
2017-08-12 12:18:59 -0400

    add message for no search results

--- src/search/list.js
+++ src/search/list.js
@@ -3,6 +3,7 @@ import Facets from './facets';
 import SearchPagination from './search_pagination';
 import MapListToggle from './maplist_toggle';
 import SearchResult from './search_result';
+import {Link} from 'react-router-dom';
 
 import './list.css';
 
@@ -10,13 +11,20 @@ import './list.css';
 class SearchListPage extends Component {
 
   render() {
-    let results = this.props.search_results.resources || [];
+    let results = this.props.search_results.resources || [],
+        noSearchResults = <div className='no-results'>
+          <p>We're sorry.  No results found for <span>"{this.props.params.query}" </span>
+            with your current filter selections.</p>
+          <p>Try clearing your filters to improve your results.</p>
+          <p><Link replace to='/search'>Show All Results</Link></p>
+          </div>;
 
     return (
     <div className='search-list'>
       <Facets {... this.props} />
       <div className='search-list-content'>
         <MapListToggle {... this.props} />
+        {results.length > 0 ? '': noSearchResults}
         <SearchPagination {... this.props} />
         <div className='search-results'>
           {results.map((res) => {
