ff85848be53ed97f27ec39cb61fa9d7517e77123
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-09-08 09:28:28 -0400

    update the favicon

---
+++ src/resources/utils.js
@@ -0,0 +1,26 @@
+import titleize from '../utils/titleize';
+
+const should_display = (str) => {
+  let parts = str.split("::");
+  return parts[0] === 'ma';
+}
+
+const strip_state = (str) => {
+  let parts = str.split("::");
+  if (should_display(str)) {
+    if (parts[0] === 'ma') {
+      return parts.slice(1).join("::");
+    } else {
+      return str;
+    }
+  } else {
+    return "";
+  }
+};
+
+const pretty_print = (str) => {
+  let parts = str.split("::");
+  return parts.map((s) => titleize(s)).join(" :: ");
+}
+
+export {strip_state, should_display, pretty_print} ;
