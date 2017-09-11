04333c4b2bd3af236ae4d3b0bc4d6a80d1b6eb0f
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-09-11 13:51:53 -0400

    update take actions page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,77 @@
-
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import MapLink from '../utils/map_link';
+import main_photo from '../images/actions/take_action.jpg'
 import photo1 from '../images/actions/eel_river_restoration.jpg'
 import photo2 from '../images/actions/education_1.jpg'
 import photo3 from '../images/actions/education_2.jpg'
 
+import DefaultLayout from './../layouts/default';
+import DatagrapherLink from '../utils/datagrapher_link';
+import Subsection from '../layouts/subsection';
+
+const ViewMap = (props) =>
+  <MapLink className='btn btn-block btn-primary' >
+    View Map
+  </MapLink>;
+
+const ViewDatagrapher = (props) =>
+  <DatagrapherLink className='btn btn-block btn-primary'>
+    View Datagrapher
+  </DatagrapherLink>;
+
+const SubNav = (props) => <ul>
+  {props.subsections.map((subsection, indx) => {
+    return <li key={indx}>
+      <Link to={"#"+subsection.id}>{subsection.name}</Link>
+    </li>
+  })}
+</ul>
+
+
 class ActionsPage extends Component {
   render() {
+    let page_image = main_photo;
+
+    let subsections = [
+      {id: "ghg_reduction",
+        name:"Rising Temperature",
+        href:"/actions/hgh-reduction-planning",
+       },
+      {id: "hazard_mitigation",
+        name:"Hazard Mitigation",
+        href:"/actions/hazard-mitigation"
+       },
+      {id: "budgeting_for_climage_change",
+        name:"Budgeting for Climate Change",
+        href:"/changes/budgeting-for-climate-change"
+       },
+      {id: "town_planning",
+        name:"Town Planning",
+        href:"/changes/town-planning"
+       }
+    ];
+
     return (
-      <div className='actions-page'>
-        <h1> Actions Page </h1>
-        <img src={photo1} className='img-fluid' alt='Eel River Restoration' title='Eel River Restoration'/>
-        <p> List all the actions here! </p>
-        <img src={photo2} className='' alt='education' title='education'/>
-        <p> We will have an image, off to the right.  <br/>
-        Maybe a set of highlighted resource results next</p>
-        <h3> Then each change will have a link to it's page </h3>
-        <p> will have a photo, some text, the top few results from that search, and then a link to search and a link to a sector page </p>
-        <img src={photo3} className='' alt='education' title='education'/>
-        <ul>
-          <li><Link to="/actions/ghg-reduction-planning"> GHG Reduction Planning </Link> </li>
-          <li><Link to="/actions/hazard-mitigation"> Hazard Mitigation </Link> </li>
-          <li><Link to="/actions/budgeting-for-climate-change"> Budgeting For Climate Change </Link> </li>
-          <li><Link to="/actions/town-planning"> Town Planning </Link> </li>
-        </ul>
-      </div>
+      <DefaultLayout
+        className='sector-index'
+        nav_actions= {[<ViewMap key={0}/> ,
+                       <ViewDatagrapher key={1} />]}
+        subnav={<SubNav subsections={subsections}/>}>
+        <h1>Identify Changes</h1>
+        <img alt='sectors example' src={page_image} className='col-12 col-md-5 float-right'/>
+
+        <p>
+          TAKE ACTION... Lorem Ipsum
+        </p>
+
+        {subsections.map((subsection, indx) => {
+          return <Subsection key={indx} {...subsection}
+                  content={ <div>
+                        <Link to={subsection.href} className='btn btn-secondary btn-block'>View {subsection.name}</Link>
+                      </div>}
+                  />})}
+      </DefaultLayout>
     );
   }
 }
