b209170091eb3aa1262a1d719487e879ef2f98b4
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-02-20 21:39:42 -0500

    adds economy sector updates

--- src/sectors/economy.js
+++ src/sectors/economy.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SectorDetailLayout from './layout';
 import sector_image from '../images/sectors/economy.jpg';
+import economy_rising_remperatures_img from '../images/sectors/economy_rising_temperatures.jpg';
+import economy_management_practices_img from '../images/sectors/economy_management_practices.jpg';
 import {searchURL} from '../search/utils';
 import {Footnote} from '../utils/footnotes';
 
@@ -23,13 +25,10 @@ class EconomySectorPage extends Component {
       <SectorDetailLayout
         title="Economy"
         image={sector_image}
-        image_credit="John Phelan"
+        image_credit="Capes Treasures, CC BY-NC-ND 2.0"
         showcased_resources={showcased_resources}
         footnotes={[
           <span>Office of Governor Charlie Baker, 2015, Letter to Request Federal Emergency Declaration – Attachment A:  2015 Severe Winter Weather Pattern Impacts - Supplemental Information. <a href='http://www.mass.gov/governor/docs/news/attachment-a-severe-winter-weather-pattern-impacts-supplemental-info.pdf'>http://www.mass.gov/governor/docs/news/attachment-a-severe-winter-weather-pattern-impacts-supplemental-info.pdf </a></span>,
-          <span> IHS Global Insights, 2015, The Economic Costs of Disruption from a Snowtstorm. <a href='https://timedotcom.files.wordpress.com/2015/01/economic-costs-of-snowstorms.pdf'>https://timedotcom.files.wordpress.com/2015/01/economic-costs-of-snowstorms.pdf</a></span>,
-          <span> NOAA, 2015, Fisheries Economics of the United States. <a href='http://www.nmfs.noaa.gov/stories/2017/04/docs/feus2015_fs_final3_web.pdf'>http://www.nmfs.noaa.gov/stories/2017/04/docs/feus2015_fs_final3_web.pdf </a></span>,
-          <span> NOAA, 2014, Gulf of Maine Atlantic Cod: 2014 Assessment Update Report, <a href='https://www.nefsc.noaa.gov/saw/cod/pdfs/GoM_cod_2014_update_20140822.pdf'>https://www.nefsc.noaa.gov/saw/cod/pdfs/GoM_cod_2014_update_20140822.pdf</a></span>,
         ]}
         sections={[
           {title: "Impacts",
@@ -40,31 +39,55 @@ class EconomySectorPage extends Component {
                 href: sectorSearchURL({changes:["ma::rising temperatures::"]}),
                 id: "impacts_rising_temperatures",
                 content: <div>
-                  <p>Rising temperatures and heat waves will likely cause more sick days due to increases in heat-related illnesses such as heat stroke and asthma, and will also increase air conditioning needs and energy costs for businesses.  Outdoor or factory-based industries such as manufacturing and home construction may also be significantly impacted by days that are too hot for workers to safely complete their tasks.</p>
-                  <p>In 2014, Massachusetts commercial fishermen sold $4.4 billion dollars of fish, directly supporting some 92,000 jobs<Footnote num='3'/>.  Rising estuarine and ocean temperatures may cause declines in certain populations of cold water fish and shellfish and the fishing jobs and other economic activities that require healthy fisheries.  Cod stocks in the Gulf of Maine dropped by 75% between 2004 and 2014<Footnote num='4'/>.  Some fresh-water fish stocks and the tourism they support may also decline.</p>
+                  <div className='section-img'>
+                    <img src={economy_rising_remperatures_img}/>
+                    <div className='image-credit'>The Daily Journal of Commerce / djcoregon.com </div>
+                  </div>
+
+                  <ul>
+                    <li>Rising temperatures and heat waves could cause more employee sick days from heat-related illnesses and higher energy costs</li>
+                    <li>Outdoor or factory-based industries such as manufacturing and home construction may face more frequent days too hot for safe work</li>
+                    <li>Warmer waters may cause declines in certain populations of cold water fish, like ocean cod, and shellfish, reducing fishing jobs and fishing-related tourism</li>
+                  </ul>
+                  <h6><a href={sectorSearchURL({changes:["ma::rising temperatures::"]})}>Find more information</a></h6>
                 </div>
               },
               { name: "Changes in Precipitation",
                 href: sectorSearchURL({changes:["ma::changes in precipitation::"]}),
                 id: 'impacts_changes_in_precipitation',
                 content: <div>
-                  <p>If water availability is affected by an increased variability in precipitation, water-dependent manufacturing industries will likely be strained, as will agricultural operations. In addition, more municipalities may need to raise water rates due to increasing prevalence of droughts and water shortages.</p>
+                  <ul>
+                    <li>Increasing prevalence of droughts and water shortages may reduce water availability for manufacturing industries and agricultural operations</li>
+                    <li>Municipalities may need to raise water rates due to strained water supplies and costs from developing new supplies</li>
+                    <li>More winter rain and less snowfall may shrink the winter sports economy </li>
+                  </ul>
+                  <h6><a href={sectorSearchURL({changes:["ma::changes in precipitation::"]})}>Find more information</a></h6>
                 </div>
               },
               { name: "Extreme Weather",
                 href: sectorSearchURL({changes:["ma::extreme weather::"]}),
                 id: 'impacts_extreme_weather',
                 content: <div>
-                  <p>Heavier, harsher storms causing floods will likely have an impact on buildings, energy delivery and transmission, and transportation systems. Many historic town centers and manufacturing areas are located in river valleys and flood plains and will be exposed to increased inundation. More frequent and extreme weather events may disrupt the supply chains for businesses dependent on specific fuels or raw materials. Similar impacts may affect product delivery. Extreme weather may also prevent workers from getting to their jobs, slowing the entire economy.</p>
-                  <p>The state’s sizable insurance and financial management sector with its 200,000 jobs may be especially affected by the growing cost of insuring against extreme weather in Massachusetts and across the country.  Steep financial losses for investors caused by climate-related impacts could also affect the state’s investment firms, while gains from new economic opportunities like renewable energy may benefit firms with smart investments in these areas.</p>
+                  <ul>
+                    <li>Severe weather may disrupt business supply chains dependent on specific fuels or raw materials, as well as product delivery</li>
+                    <li>Extreme weather may also prevent workers from getting to their jobs, slowing the entire economy</li>
+                    <li>Flooding could damage business facilities, large manufacturing areas in river valleys, energy delivery and transmission, and transportation systems</li>
+                    <li>The state’s insurance and financial management sector (200,000 jobs) may be affected by the growing cost of insuring against extreme weather</li>
+                  </ul>
+                  <h6><a href={sectorSearchURL({changes:["ma::extreme weather::"]})}>Find more information</a></h6>
                 </div>
               },
               { name: "Sea Level Rise",
                 href: sectorSearchURL({changes:["ma::sea level rise::"]}),
                 id: 'impacts_slr',
-                content: <p>
+                content: <div>
                   Much of the Commonwealth’s critical infrastructure, not to mention homes and businesses, lie on the coast and must be prepared for the eventuality of sea-level rise. If not, encroaching salt water and higher incidence of flooding could slow the economy by damaging transportation and utility infrastructure, as well as goods and properties.
-                </p>
+                  <ul>
+                    <li>Sea level rise is likely to increase coastal hazards like flooding for many businesses and for critical infrastructure– such as transportation and utility infrastructure</li>
+                    <li>Encroaching salt water and chronic flooding could necessitate costly relocation for some businesses</li>
+                  </ul>
+                  <h6><a href={sectorSearchURL({changes:["ma::sea level rise::"]})}>Find more information</a></h6>
+                </div>
               }
             ]},
             {title:"Actions",
@@ -74,41 +97,69 @@ class EconomySectorPage extends Component {
                {name: "Planning",
                 href: sectorSearchURL({actions:["ma::planning::"]}),
                 id: 'actions_planning',
-                content: <ul>
-                  <li>Assess the ability to meet health care capacity needs, especially for business operations that are vulnerable to flooding, extreme heat, or poor air quality.</li>
-                  <li>Encourage businesses to take account of climate change impacts in risk assessments and risk minimization strategies, particularly as data are refined to a sub-state level.</li>
-                  <li>Maintain and develop emergency management plans with support from the Massachusetts Emergency Management Agency (MEMA) and the Federal Emergency Management Agency (FEMA).</li>
-                </ul>
+                content: <div>
+                  <ul>
+                    <li>Encourage businesses to take account of climate change impacts in risk assessments and risk minimization strategies</li>
+                    <li>Develop local emergency management plans with support from the Massachusetts Emergency Management Agency (MEMA)</li>
+                    <li>Assess capacity to meet the needs of employees who are vulnerable to health impacts from flooding, extreme heat, or poor air quality</li>
+                  </ul>
+                  <h6><a href={sectorSearchURL({changes:["ma::planning::"]})}>Find more information</a></h6>
+                </div>
               },
               {name: "Management Practices",
                 href: sectorSearchURL({actions:["ma::implementation action/direct action on target::management and behavior"]}),
                 id: 'actions_management_practices',
-                content: <ul>
-                  <li> Avoid siting operations or businesses in flood prone areas. For existing operations in these areas, support building design modifications, such as elevating electrical and HVAC systems above ground level, and installing systems that can withstand frequent flooding and heat waves.</li>
-                  <li> Consider altering operation schedules to cooler times of day when possible.</li>
-                  <li> Support improvements to stormwater management at business facilities to reduce runoff and its water quality impacts, and expand opportunities for water recycling, including grey water.</li>
-                  <li> Examine advantages of increasing inventory supplies and identify alternative materials and inventory suppliers to avoid delivery interruptions.</li>
-                  <li> Investigate alternate transportation routes, energy supplies, and communication systems for suppliers, customers, and workers to respond to evolving climate change impacts.</li>
-                  <li> Provide outreach and educate public and private employers about Occupational Safety and Health Administration (OSHA) requirements regarding employee protections in times of high heat.</li>
-                  <li> Consider using renewable energy in business operations, performing energy efficiency retrofits of facilities, and looking for opportunities to conserve energy in business processes and in employee commuting.</li>
-                </ul>
+                content:
+                <div>
+                  <div className='section-img'>
+                    <img src={economy_management_practices_img}/>
+                    <div className='image-credit'>Team Massachusetts 4D, CC BY 2.0</div>
+                  </div>
+                  <ul>
+                    <li>Consider using renewable energy, performing energy efficiency retrofits, and conserving energy in business processes and in employee commuting</li>
+                    <li>Avoid siting new business facilities in flood prone areas</li>
+                    <li>Ensure businesses in potential flood zones are flood-proofed, for example by elevating electrical and HVAC systems above ground level</li>
+                    <li>Improve storm water management at business facilities and conserve water via grey water systems</li>
+                    <li>Identify alternative materials and inventory suppliers to avoid delivery interruptions due to extreme weather or other events</li>
+
+                    <li>Investigate alternate transportation routes and communication systems for suppliers, customers, and workers to use during emergencies</li>
+                    <li>Educate employers about OSHA requirements for employee protections in times of high heat</li>
+                  </ul>
+                  <h6><a href={sectorSearchURL({actions:["ma::implementation action/direct action on target::management and behavior"]})}>Find more information</a></h6>
+                </div>
               },
              {name: "Technology",
               href: sectorSearchURL({actions:["ma::implementation action/direct action on target::technology"]}),
               id:'actions_technology',
-              content: <ul>
-                <li> Consider improvements to the structural stability, water drainage systems, and weatherproofing of critical infrastructure or other economically vital sites as part of capital improvement projects.</li>
-                <li> Investigate installation of year-round climate control assets, including natural site-specific climate control assets such as tree cover and vegetation, and re-examine the thermal properties of buildings to increase resilience and reduce greenhouse gas production.</li>
-                <li> Seek to reduce water use in energy production by considering alternative technologies, since peak water use in energy production often coincides with periods of high heat and low water availability.</li>
-              </ul>
+              content: <div>
+                <ul>
+                    <li>Consider improvements to the structural stability, water drainage, and weatherproofing of critical infrastructure and other economically vital sites as part of capital improvement projects.</li>
+                    <li>Enhance natural, climate control assets such as tree cover and vegetation</li>
+                    <li>Install energy efficient technologies and building systems to reduce energy use</li>
+                    <li>Ensure remote access and teleworking options are available for employees during extreme weather, flooding and other storm events</li>
+                    <li>Set up emergency communication notification systems so employees know of office closures or evacuations</li>
+                  </ul>
+                  <h6>
+                    <a href={sectorSearchURL({actions:["ma::implementation action/direct action on target::technology"]})}>
+                      Find more information
+                    </a>
+                   </h6>
+               </div>
               },
               { name: "Policies / Laws",
                 href: sectorSearchURL({actions:["ma::planning::policies/laws/regulations"]}),
                 id:'actions_policies_laws',
-                content: <ul>
-                  <li>Establish partnerships between industry and government to facilitate technical assistance to businesses that are adapting to climate change.</li>
-                  <li>Encourage or incentivize conversion to more energy-efficient buildings and processes and to local renewable energy sources to reduce dependence on the electric grid and to build resilience for power disruptions that can occur during extreme weather events.</li>
-                </ul>
+                content: <div>
+                  <ul>
+                    <li>Establish partnerships between industry and government to provide technical assistance to businesses for adapting to climate impacts</li>
+                    <li>Encourage or incentivize energy efficiency and  renewable energy to reduce dependence on the electric grid and build resilience for power disruptions</li>
+                  </ul>
+                  <h6>
+                    <a href={sectorSearchURL({actions:["ma::planning::policies/laws/regulations"]})}>
+                      Find more information
+                    </a>
+                   </h6>
+                </div>
               }]},
           {title: "Looking for help?",
             id: 'looking_for_help',
@@ -117,14 +168,14 @@ class EconomySectorPage extends Component {
             </ul>,
             subsections: []
           }
-
         ]}
         >
           <div>
-            <p>Massachusetts has a strong and growing economy, backed by a network of 125 colleges and universities that make the Commonwealth a world leader in technology, science, and health care.  In 2017, Massachusetts was ranked the best state by U.S. News and World Report based on its strong economy, excellent health care, and well-performing education system.  </p>
-            <p>Against this vibrant back drop, climate change will produce serious risks as well as new opportunities for many Massachusetts businesses— including businesses in major and growing sectors of the economy such as health care, technology, education, information technology, financial services, manufacturing, and renewable energy.</p>
-            <p>Investment in the development of climate change solutions provide opportunities that the Commonwealth is uniquely suited to pursue given the State’s focus on research and technology.  Massachusetts is already a leader in the transition to a climate-friendly, clean energy economy.  More than 105,000 people are now employed in the clean energy industry—up 75 percent since 2010, and those numbers continue to grow. </p>
-            <p>Climate change will also increase exposure to risks and challenges across every economic sector. Extreme rain and snow storms can cost hundreds of millions of dollars from lost sales, increased fuel and supply prices, and direct damage and clean-up costs. The string of winter storms in February 2015 cost the Commonwealth more than $35 million alone <Footnote num='1'/>, and a full economic shutdown from an extreme weather event would cost approximately $265 million <em>per day</em><Footnote num='1'/>.  </p>
+            <p>In 2017, the Commonwealth earned a ranking in U.S. News and World Report as the #1 best place to live in the United States due to its strong economy, excellent health care system and top-notch education system.</p>
+
+            <p>Climate change will produce some serious challenges for our thriving economy. Warmer temperatures could cause declines in fisheries, heat waves could slow outdoor work and increase power outages, and mega storms could decimate business operations. The string of winter storms in February 2015 alone cost the Commonwealth more than $35 million.  <Footnote num='1'/></p>
+
+            <p>Massachusetts is already a leader in the transition to a climate-friendly, clean energy economy. More than 105,000 people are now employed in the clean energy industry. As the transition gains steam, these jobs will continue to grow.</p>
           </div>
         </SectorDetailLayout>
     );
--- src/welcome_modal.js
+++ src/welcome_modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
 import {withCookies} from 'react-cookie';
+import ma_gov_seal from './images/ma_gov_seal.png';
 import './welcome_modal.css';
 
 Modal.setAppElement("#root");
@@ -8,7 +9,6 @@ Modal.setAppElement("#root");
 class WelcomeModal extends Component {
   constructor() {
     super();
-    console.log(this.props);
     this.state = {
       open_welcome_modal: true
     };
@@ -25,10 +25,22 @@ class WelcomeModal extends Component {
       isOpen={this.state.open_welcome_modal && !seen_before}
       >
         <h1> Welcome </h1>
+        <img src={ma_gov_seal}/>
         <p>
-           Content coming soon...
+          I am pleased to release the resilient MA Climate Clearinghouse.  This website, and the Municipal Vulnerability Preparedness (MVP) program it supports, are cornerstones of our administration’s efforts to further reduce greenhouse gas emissions, safeguard residents, municipalities and businesses from the impacts of climate change, and build a more resilient Commonwealth. While a changing climate knows no geographical boundaries, cities and towns across Massachusetts are on the front lines of climate change. Launched in 2017, the MVP program grew out of a strong desire to work with cities and towns to prepare for the impacts of climate change and build more resilient communities. We are proud that in our first year 71 communities have joined the program and are taking a leadership role using their local knowledge and community strengths to partner with the Commonwealth on this issue.
         </p>
-        <div className='btn btn-block btn-primary' onClick={() => this.close_modal()}> Close </div>
+
+        <p>
+            As first announced when we hosted a Municipal Climate Change Summit, our administration has produced the resilient MA Climate Clearinghouse to ensure continued access to information and enable communities to access the best science and data on expected climate changes, information on planning and actions to support community resiliency for climate change, and links to important grant programs and technical assistance.  The site also catalogs specific vulnerabilities, risks and strategies for different sectors including agriculture, forestry, local government, education, energy, recreation, and transportation. All of the climate projections included on the website are specific to Massachusetts and were produced by the Northeast Climate Science Center at UMass-Amherst. We’ve also included an interactive map so that users can understand how climate change will affect their specific location and the resources they manage.
+
+        </p>
+        <p>
+            From the Berkshires to the Cape and Islands many communities are already working hard and leading the way to build resilience and we are excited to capture your ideas, strategies and best practices in future versions of this website. We hope you will find our Climate Clearinghouse to be useful tool, whether you are working on your MVP plan, applying for a grant to support a new capital project, or educating a local community group on the impacts of climate change.
+        </p>
+        <p>
+          We look forward to working together to build resilient and strong communities across our shared Commonwealth.
+        </p>
+        <div className='btn btn-block btn-primary' onClick={() => this.close_modal()}> Continue </div>
       </Modal>
   }
 }
