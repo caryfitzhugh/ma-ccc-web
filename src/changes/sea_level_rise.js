5c932a051bc89eca25e5ddb1bc4cd71f08a8784d
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-02-27 15:35:57 -0500

    slr page updates

--- src/changes/sea_level_rise.js
+++ src/changes/sea_level_rise.js
@@ -34,29 +34,66 @@ class SeaLevelRiseChangesPage extends Component {
            href: searchURL({facets: { climate_changes: ["ma::sea level rise::"]}}),
            id: "sea_level_rise_projected_changes",
            content: <div>
-            <p>We have data at several established tide gauge stations with long-term records for historic and current sea level trends along the Massachusetts coast. Researchers from the Northeast Climate Science Center at UMass Amherst have developed probabilistic projections to show how much and how quickly future mean sea level is likely to rise at these gauges under two future atmospheric greenhouse gas emission scenarios (referred to as representative concentration pathways [RCP]). There is little variability in the projections among different stations.</p>
+              <p>We have data at several established tide gauge stations with long-term records for historic and current sea level trends along the Massachusetts coast. Researchers from the Northeast Climate Science Center at UMass Amherst have developed probabilistic projections to show how much and how quickly future mean sea level is likely to rise at these gauges under two future atmospheric greenhouse gas emission scenarios (referred to as representative concentration pathways [RCP]). There is little variability in the projections among different stations.</p>
 
-            <div style={{width: '80%', margin: '5% 10%'}} >
-              <img alt='chart of SLR projections' style={{width: "100%"}} src={slr_chart}></img>
-            </div>
+              <table className='table chart t-c' style={{textAlign: "center"}}>
+                  <thead>
+                    <tr className='blue'>
+                      <th colSpan='6'> <h5>Relative mean sea level (feet NAVD88) for Woods Hole, MA</h5> </th>
+                    </tr>
+                    <tr className='grey'>
+                      <th> Scenario </th>
+                      <th> Probabilistic Projections </th>
+                      <th> 2030 </th>
+                      <th> 2050 </th>
+                      <th> 2070 </th>
+                      <th> 2100 </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className='grey'> Intermediate </td>
+                      <td> Unlikely to exceed (83% probability) given a high emissions pathway (RCP 8.5)</td>
+                      <td> 0.6 </td>
+                      <td> 1.3 </td>
+                      <td> 2.3 </td>
+                      <td> 4.0 </td>
+                    </tr>
+                    <tr>
+                      <td className='grey'> Intermediate - High</td>
+                      <td> Extremely unlikely to exceed (95% probability) given a high emissions pathway (RCP 8.5)</td>
+                      <td> 0.8 </td>
+                      <td> 1.7 </td>
+                      <td> 2.9 </td>
+                      <td> 5.1 </td>
+                    </tr>
+                    <tr>
+                      <td className='grey'> High</td>
+                      <td> Extremely unlikely to exceed (99.5% probability) given a high emissions pathway (RCP 8.5)</td>
+                      <td> 1.1 </td>
+                      <td> 2.4 </td>
+                      <td> 4.2 </td>
+                      <td> 7.7 </td>
+                    </tr>
+                    <tr>
+                      <td className='grey'> Extreme <br/> (Maximum physically plausible)</td>
+                      <td> Exceptionally unlikely to exceed (99.9% probability) given a high emissions pathway (RCP 8.5)</td>
+                      <td> 1.3 </td>
+                      <td> 3.1 </td>
+                      <td> 5.4 </td>
+                      <td> 10.3 </td>
+                    </tr>
+                  </tbody>
+              </table>
 
-            <div style={{width: '80%', margin: '5% 10%'}} >
-              <img alt='graph of SLR projections' style={{width: "100%"}} src={slr_graph}></img>
-              <h6>Relative (or local) mean sea level projections for the Woods Hole, MA tide station based on four National Climate Assessment global scenarios with associated probabilistic model outputs from the Northeast Climate Science Center. Each of the scenarios—Intermediate, Intermediate-High, High, and Extreme—is cross-walked with two to three probabilistic model outputs. Modeling considered two future concentrations of greenhouse gas emissions (referred to as representative concentration pathways [RCP]) and two methods of accounting for Antarctic ice sheet contributions to sea level rise. A 19-year reference time period for sea level (tidal epoch) centered on the year 2000 was used to minimize biases caused by tidal, seasonal, and inter-annual climate variability. Sea level projections for the Woods Hole tide station are referenced to the North American Vertical Datum of 1988 (NAVD88).</h6>
-            </div>
+              <div style={{width: '100%', margin: '5% 0%'}} >
+                <img alt='graph of SLR projections' style={{width: "100%"}} src={slr_graph}></img>
+                <h6 className='chart-subtitle'>Relative (or local) mean sea level projections for the Woods Hole, MA tide station based on four National Climate Assessment global scenarios with associated probabilistic model outputs from the Northeast Climate Science Center. Each of the scenarios—Intermediate, Intermediate-High, High, and Extreme—is cross-walked with two to three probabilistic model outputs. Modeling considered two future concentrations of greenhouse gas emissions (referred to as representative concentration pathways [RCP]) and two methods of accounting for Antarctic ice sheet contributions to sea level rise. A 19-year reference time period for sea level (tidal epoch) centered on the year 2000 was used to minimize biases caused by tidal, seasonal, and inter-annual climate variability. Sea level projections for the Woods Hole tide station are referenced to the North American Vertical Datum of 1988 (NAVD88).</h6>
+              </div>
 
-
-            We have data for historic and current sea levels from tide gauge stations along the Massachusetts coast.  Researchers from the Northeast Climate Science Center at UMass Amherst have developed projections to show how much and how quickly average sea levels are likely to rise at these gauges under different emissions scenarios.  There is little variability in the projections among different stations.
-            <div style={{width: '80%', margin: '5% 10%'}} >
-              <img alt='graph of SLR projections' style={{width: "100%"}} src={slr_graph}></img>
-              <h6>Figure 1: Sea level rise projections at the Nantucket tide gauge site. Projection curves are given for the medium (RCP 4.5) and high (RCP 8.5) emissions scenarios, at multiple levels of likelihood, in feet relative to mean sea level in 2000. These projections of sea level rise do not include potential contributions from significant losses of the Antarctic ice sheet.  </h6>
-            </div>
-
-            <p>By 2100, sea level rise is projected to range between 2.2 to 4.3 feet in the Nantucket area under a high emissions scenario.  These Massachusetts sea level rise projections do not include potential contributions from significant losses of the Antarctic ice sheet.  </p>
-            <img alt='img of slr map' className='col-md-6 col-sm-12' style={{float: 'right'}} src={slr_image}></img>
-            <p>For low elevation coastal areas, even a rise of less than a foot can produce significant new risks for development and infrastructure such as the electrical grid and storm and wastewater systems near the shore.  Sea level rise is already causing more frequent coastal flooding in low lying areas across the world</p>
-
-            <p>Sea level rise driven by climate change will exacerbate many other existing coastal hazards, like severe storms and storm surge, tidal inundation and salt water intrusion, which could produce billions of dollars of impacts for Massachusetts.</p>
+              <p>By 2100, mean sea level could reach 4.0 to 10.3 feet (NAVD88) in the Woods Hole area given a high emissions pathway. </p>
+              <p>For low elevation coastal areas, even a rise of a foot can produce new risks for development and infrastructure such as electrical, stormwater and wastewater systems. Sea level rise is already causing more frequent coastal flooding in low lying areas across the coast of Massachusetts.</p>
+              <p>Sea level rise driven by climate change will exacerbate many other existing coastal hazards, like severe storms and storm surge, tidal inundation and salt water intrusion, which could produce billions of dollars of impacts for Massachusetts.</p>
            </div>,
            subsections: []
           },
