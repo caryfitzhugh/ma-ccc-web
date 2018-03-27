bd1b4fafa7e5603e866292da1961e42cffde285a
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-03-27 10:03:10 -0400

    mvp page updates

--- src/mvp_page.js
+++ src/mvp_page.js
@@ -18,7 +18,7 @@ class MVPPage extends Component {
           <div className='container'>
             <SectorDetailLayout
               image={mvp_image}
-              image_credit='Scituate, Mass.  Released photo by U.S. National Guard'
+              image_credit='Scituate, Mass. Released photo by U.S. National Guard'
               title="Municipal Vulnerability Preparedness"
               showcased_resources={showcased_resources}
               datagrapher_file='Temp/state/pcpn/ANN/MA/'
@@ -27,20 +27,20 @@ class MVPPage extends Component {
               sections={[
                 { title: "How do I become an MVP community?",
                   id: "resources",
-                  content: <p>
-                    <p>Applications for the 2018 Urgent Need Round are due by 5:00 p.m. <strong>Monday, May 21st, 2018</strong></p>
-                    <p>All cities and towns in the Commonwealth are eligible to apply.  MVP designation will be given to municipalities who complete a Community Resilience Building workshop process to prepare for the impacts of climate change and fulfill associated deliverables required to maintain designation status.</p>
+                  content: <div>
+                    <p>Applications for the 2018 Planning Grants are due by 5:00 p.m. <strong>Friday, May 11th, 2018</strong></p>
+                    <p>All cities and towns in the Commonwealth are eligible to apply. MVP designation will be given to municipalities who complete a Community Resilience Building workshop process to prepare for the impacts of climate change and fulfill associated deliverables required to maintain designation status.</p>
                     <p>Application Requirements:</p>
-                      <ul>
-                        <li>A signed letter of support from the chair of the board of selectmen, mayor, a town administrator, or similar city or town official.</li>
-                        <li>A short statement of the municipality’s commitment to taking on this grant and planning for the impacts of climate change.  Please also outline the municipality’s commitment to providing sufficient staff time match to complete the project.</li>
-                        <li>The name and qualifications (please include resume and summary of experience) of an employee of the municipality, a committee member, or volunteer who can serve as the local project manager, assemble a core team to work on the project, and point of contact for the grant.</li>
-                        <li>A summary statement of municipal support, including letters of support indicating interest in participating in the planning process from all relevant local boards, departments, commissions, businesses, organizations and other partners, including local, regional and statewide agencies or groups focused on climate change adaptation, natural resources and conservation.</li>
-                        <li>A description of how the municipality will use the results of this process to inform ongoing or new planning efforts including local hazard mitigation plans, open space plans, master plans, etc., and how results from this process would be used to inform that plan.</li>
-                        <li>Please tell us about your need to address climate change.  What are the expected climate change impacts in your community and what do you think are the biggest risks?</li>
-                        <li>Municipalities may request to expand the scope of the planning grant to include additional tasks such as follow-on workshops or public listening sessions, targeted vulnerability assessments of critical sectors to gather more data, or additional steps they believe would build on the MVP planning process and further advance their community resiliency building efforts.  Please describe the work you intend to do to build on the MVP planning process, how it fits in with the overall MVP planning process as you understand it from your review of the <a href='https://www.communityresiliencebuilding.com/'>Community Resilience Building Guide</a>, the timeline for which you would complete it in, and the outcomes you expect to achieve.</li>
+                    <ul>
+                      <li>A signed letter of support from the chair of the board of selectmen, mayor, a town administrator, or similar city or town official.</li>
+                      <li>A short statement of the municipality’s commitment to taking on this grant and planning for the impacts of climate change. Please also outline the municipality’s commitment to providing sufficient staff time match to complete the project.</li>
+                      <li>The name and qualifications (please include resume and summary of experience) of an employee of the municipality, a committee member, or volunteer who can serve as the local project manager, assemble a core team to work on the project, and point of contact for the grant.</li>
+                      <li>A summary statement of municipal support, including letters of support indicating interest in participating in the planning process from all relevant local boards, departments, commissions, businesses, organizations and other partners, including local, regional and statewide agencies or groups focused on climate change adaptation, natural resources and conservation.</li>
+                      <li>A description of how the municipality will use the results of this process to inform ongoing or new planning efforts including local hazard mitigation plans, open space plans, master plans, etc., and how results from this process would be used to inform that plan.</li>
+                      <li>Please tell us about your need to address climate change. What are the expected climate change impacts in your community and what do you think are the biggest risks?</li>
+                      <li>Municipalities may request to expand the scope of the planning grant to include additional tasks such as follow-on workshops or public listening sessions, targeted vulnerability assessments of critical sectors to gather more data, or additional steps they believe would build on the MVP planning process and further advance their community resiliency building efforts. Please describe the work you intend to do to build on the MVP planning process, how it fits in with the overall MVP planning process as you understand it from your review of the <a href='https://www.communityresiliencebuilding.com/'>Community Resilience Building Guide</a>, the timeline for which you would complete it in, and the outcomes you expect to achieve.</li>
                     </ul>
-                  </p>,
+                  </div>,
                   subsections: []
               },
               { title: "Do you live in an MVP community?",
@@ -59,18 +59,15 @@ class MVPPage extends Component {
               }
 
               ]}>
-
                 <p>The Baker-Polito Administration’s Municipal Vulnerability Preparedness (MVP) grant program provides support for cities and towns across the Commonwealth to identify climate change vulnerabilities, prioritize critical actions, and build community resiliency.</p>
 
-                <p>Building on the Administration’s approach to state and local partnerships, the MVP program awards municipalities with funding and technical support to complete a community-led planning process to:
-                  <ul>
-                    <li>Define extreme weather and natural and climate related hazards</li>
-                    <li>Identify existing and future vulnerabilities and strengths</li>
-                    <li>Develop and prioritize actions and opportunities for the communities to reduce risk and build resilience</li>
-                  </ul>
-                </p>
-                <p>Once a municipality has completed the planning process they become eligible for follow-on funding opportunities, including MVP implementation grants, and advanced standing in other grant opportunities.</p>
-
+                <p>Building on the Administration’s approach to state and local partnerships, the MVP program awards municipalities with funding and technical support to complete a community-led planning process to:</p>
+                <ul>
+                  <li>Define extreme weather and natural and climate change related hazards</li>
+                  <li>Identify existing and future community vulnerabilities and strengths</li>
+                  <li>Develop and prioritize actions and opportunities to reduce risk and build resilience</li>
+                </ul>
+                <p>Once a municipality has completed the planning process they become eligible for follow-on funding opportunities, including MVP action grants, and advanced standing in other grant opportunities.</p>
               </SectorDetailLayout>
             </div>
           <Footer/>
