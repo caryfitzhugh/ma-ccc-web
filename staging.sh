039f6c347abd5fed104d91b20b3162a7d5fe6bd6
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-06-18 23:49:01 -0400

    update staging code

--- staging.sh
+++ staging.sh
@@ -4,3 +4,4 @@ npm run build
 aws s3 sync --profile=nescaum build/ s3://ma-web-nescaum-ccsc-dataservices --acl public-read
 aws cloudfront create-invalidation --profile=nescaum --distribution-id E19XY2FPLOWT0A --paths "/*"
 rm -rf build/
+echo "https://d11m2g9w3nd649.cloudfront.net"
