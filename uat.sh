9d815d0509ed9ab0d7dab601419011df18d532aa
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-07-05 21:22:59 -0400

    fixes the growing image issue. on ohover

--- uat.sh
+++ uat.sh
@@ -1,4 +1,4 @@
 rm -rf build/
 npm run build
 
-aws s3 sync --profile=mass-uat build/ s3://eea-usersworkspace/home/Cary.FitzHugh/mass-web
+aws s3 sync --profile=mass-uat build/ s3://eea-usersworkspace/home/Cary.FitzHugh/mass-web --acl public-read
