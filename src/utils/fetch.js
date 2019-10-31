f71a1b125d7ddf4b4217695f39bfa4147c35dda2
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-10-31 16:34:31 -0400

    fetch the HTTPS version of the API

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,4 +1,4 @@
-const API_HOST = 'http://d2iq7uivdprspu.cloudfront.net'
+const API_HOST = 'https://d2iq7uivdprspu.cloudfront.net'
 const API_POST_HOST = 'https://repository.nescaum-ccsc-dataservices.com';
 const STATE = "MA";
 export { API_POST_HOST, API_HOST, STATE };
