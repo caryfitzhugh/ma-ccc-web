fda8787450386c45bb15785454706f52fb4da5f2
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-12-18 22:04:17 -0500

    updated header

--- src/header.js
+++ src/header.js
@@ -87,7 +87,7 @@ class MegaMenu extends Component {
                     <h3>State Hazard Mitigation and Climate Adaptation Plan:</h3>
                     <ul>
                       <li><a href='/shmcap-portal/index.html' onClick={(e) => window.location.href = e.target['href']} >Our Plan </a></li>
-                      <li><a href='/state-action-tracker/index.html' onClick={(e) => window.location.href = e.target['href']} >Massachusetts State Action Tracker </a></li>
+                      <li><a href='/shmcap-portal/index.html#/action-tracker/' onClick={(e) => window.location.href = e.target['href']} >Massachusetts State Action Tracker </a></li>
                     </ul>
                   </section>
                   <section className='col col-sm-4 no-pad-r'>
