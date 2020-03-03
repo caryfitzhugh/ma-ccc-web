6926ec065ada34650fd2c374dfc6504cb555d615
Cary FitzHugh <cary.fitzhugh@gmail.com>
2020-03-03 12:47:51 -0500

    update resource remapping

--- src/utils/resource_remapping.js
+++ src/utils/resource_remapping.js
@@ -1,7 +1,7 @@
 const ResourceRemapping = {
   "Municipal Vulnerability Preparedness (MVP)":  "/mvp",
-  "Executive Summary of the Massachusetts State Hazard Mitigation and Climate Adaptation Plan" : "/shmcap-portal",
+  "Executive Summary of the Massachusetts State Hazard Mitigation and Climate Adaptation Plan" : "/shmcap-portal/index.html",
 
-  "Massachusetts State Hazard Mitigation and Climate Adaptation Plan (SHMCAP)": "/shmcap-portal"
+  "Massachusetts State Hazard Mitigation and Climate Adaptation Plan (SHMCAP)": "/shmcap-portal/index.html"
 }
 export {ResourceRemapping};
